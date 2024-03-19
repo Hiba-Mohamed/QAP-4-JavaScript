fad63e35fadcd06d2140de458f394ab69d4499a1
Hiba-Mohamed <hibamoha987@gmail.com>
2024-03-19 13:10:36 -0230

    embedding html from the js file

--- JSproject/JSQAP4.js
+++ JSproject/JSQAP4.js
@@ -4,7 +4,7 @@ const motelCustomer = {
 
     birthDate:{
         birthYear: 1997,
-        birthMonth: 12,
+        birthMonth: 2,
         birthday: 8,
     },
 
@@ -33,24 +33,50 @@ const motelCustomer = {
 
     checkOutDate: {
         checkOutYear: 2024,
-        checkOutMonth: 2,
+        checkOutMonth: 6,
         checkOutDay: 1,
         checkOutTime: "15:00"
     },
 
-    getAge: function(){
-        const today = new Date()
-        const birthDate = new Date(motelCustomer.birthDate.birthYear, motelCustomer.birthDate.birthMonth - 1, motelCustomer.birthDate.birthday);
+    getAge: function() {
+        const today = new Date();
+        const birthDate = new Date(
+            this.birthDate.birthYear,
+            this.birthDate.birthMonth - 1,
+            this.birthDate.birthday
+        );
         let age = today.getFullYear() - birthDate.getFullYear();
-        if (birthDate.birthMonth - 1 <  today.getMonth()){
-            age -= 1
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+        // check if the birth month or day had passed on not
+        if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+            age--;
         }
         return age;
     },
 
     getDurationOfStay: function(){
-
+        const formattedCheckIn = new Date (motelCustomer.checkInDate.checkInYear, motelCustomer.checkInDate.checkInMonth - 1, motelCustomer.checkInDate.checkInDay);        
+        const formattedCheckOut = new Date (motelCustomer.checkOutDate.checkOutYear, motelCustomer.checkOutDate.checkOutMonth - 1, motelCustomer.checkOutDate.checkOutDay);        
+        if (formattedCheckIn > formattedCheckOut)
+            {
+                console.log("Ckeckin date is later than the check out date")
+            }
+        return formattedCheckOut - formattedCheckIn
     }
-
- 
 }
+
+const customerAge = motelCustomer.getAge();
+
+html = `
+  <ul>
+  <center>
+  <h2>Motel Customer</h2>
+    <li>Name: ${motelCustomer.customerName} </li>
+    <li>Birthdate: ${motelCustomer.birthDate.birthYear +"/"+ motelCustomer.birthDate.birthMonth +"/"+ motelCustomer.birthDate.birthday} </li>
+    <li>Age: ${customerAge} </li>
+    </center>
+  </ul>
+`;
+
+console.log(html);
+document.body.innerHTML = html;
